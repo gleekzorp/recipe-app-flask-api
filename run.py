7160dca6d3902a0f70af4c78a4e7444b417a8bc6
gleekzorp <gleek.dan@gmail.com>
2019-04-01 19:18:16 -0600

    Final Commit Ready for Heroku PUSH.  I will work on React side now

--- run.py
+++ run.py
@@ -1,25 +1,10 @@
-# Use this for local testing
-# Don't forget to setup local environment configs
 import os
-
 from src.app import create_app
 
+env_name = os.getenv('FLASK_ENV')
+app = create_app(env_name)
+
 if __name__ == '__main__':
-  env_name = os.getenv('FLASK_ENV')
-  app = create_app(env_name)
+  port = os.getenv('PORT')
   # run app
-  app.run()
-
-
-
-# Use this for Heroku Deploy
-# import os
-# from src.app import create_app
-
-# env_name = os.getenv('FLASK_ENV')
-# app = create_app(env_name)
-
-# if __name__ == '__main__':
-#   port = os.getenv('PORT')
-#   # run app
-#   app.run(host='0.0.0.0', port=port)
+  app.run(host='0.0.0.0', port=port)
